885696c71802d110bf566e84b68fbabf837f8d2a
CelebornBrian <pkbuer@stud.ntnu.no>
2018-10-16 14:55:50 +0200

    Created the router, based on Assignment 1, added regex and switch options

---
+++ router.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"regexp"
+)
+
+//nolint: gocyclo
+func router(w http.ResponseWriter, r *http.Request) {
+	//Build regex expressions for the url and handle possible errors
+	redirectApi, err := regexp.Compile("^/paragliding/?$")
+	if err != nil {
+		errStatus(w, http.StatusInternalServerError, err, "Failed to compile redirect regex")
+		return
+	}
+	apiHandler, err := regexp.Compile("^/paragliding/api/?$")
+	if err != nil {
+		errStatus(w, http.StatusInternalServerError, err, "Failed to compile api regex")
+		return
+	}
+	apiTrackHandler, err := regexp.Compile("^/paragliding/api/track/?$")
+	if err != nil {
+		errStatus(w, http.StatusInternalServerError, err, "Failed to compile api/track regex")
+		return
+	}
+	apiTrackIDHandler, err := regexp.Compile("^/paragliding/api/track/[0-9]+/?$")
+	if err != nil {
+		errStatus(w, http.StatusInternalServerError, err, "Failed to compile api/igc/<id> regex")
+		return
+	}
+	apiTrackIDFieldHandler, err := regexp.Compile("/paragliding/api/track/[0-9]+/(pilot|glider|glider_id|track_length|H_date)$")
+	if err != nil {
+		errStatus(w, http.StatusInternalServerError, err, "Failed to compile api/track/<id>/<field> regex")
+		return
+	}
+	apiTicker, err := regexp.Compile("/paragliding/api/ticker/?$")
+	if err != nil {
+		errStatus(w, http.StatusInternalServerError, err, "Failed to compile api/ticker regex")
+		return
+	}
+	apiTickerLatest, err := regexp.Compile("/paragliding/api/ticker/latest/?$")
+	if err != nil {
+		errStatus(w, http.StatusInternalServerError, err, "Failed to compile api/ticker/latest regex")
+		return
+	}
+	apiTickerTimestamp, err := regexp.Compile("/paragliding/api/ticker/latest/[0-9]+/?$")
+	if err != nil {
+		errStatus(w, http.StatusInternalServerError, err, "Failed to compile api/ticker/timestamp regex")
+		return
+	}
+	apiWebhookNewTrack, err := regexp.Compile("/paragliding/api/webhook/new_track/?$")
+	if err != nil {
+		errStatus(w, http.StatusInternalServerError, err, "Failed to compile api/webhook regex")
+		return
+	}
+	apiWebhookID, err := regexp.Compile("/paragliding/api/webhook/new_track/id/?$")
+	if err != nil {
+		errStatus(w, http.StatusInternalServerError, err, "Failed to compile api/webhook/id regex")
+		return
+	}
+	adminApiTrackCount, err := regexp.Compile("/paragliding/admin/api/tracks_count/?$")
+	if err != nil {
+		errStatus(w, http.StatusInternalServerError, err, "Failed to compile admin/api/track_count regex")
+		return
+	}
+	adminApiTracks, err := regexp.Compile("/paragliding/admin/tracks/?$")
+	if err != nil {
+		errStatus(w, http.StatusInternalServerError, err, "Failed to compile admin/api/tracks regex")
+	}
+
+	//Check if request is GET, POST or DELETE
+	if r.Method == http.MethodGet || r.Method == http.MethodPost || r.Method == http.MethodDelete {
+		//Switch on the request url path and select handler
+		switch {
+		case redirectApi.MatchString(r.URL.Path):
+			http.Redirect(w, r, "/paragliding/api", http.StatusPermanentRedirect)
+		case apiHandler.MatchString(r.URL.Path):
+			handleAPI(w)
+		case apiTrackHandler.MatchString(r.URL.Path):
+			if r.Method == http.MethodPost {
+
+			} else if r.Method == http.MethodGet {
+
+			}
+		case apiTrackIDHandler.MatchString(r.URL.Path):
+		case apiTrackIDFieldHandler.MatchString(r.URL.Path):
+		case apiTicker.MatchString(r.URL.Path):
+		case apiTickerLatest.MatchString(r.URL.Path):
+		case apiTickerTimestamp.MatchString(r.URL.Path):
+		case apiWebhookNewTrack.MatchString(r.URL.Path):
+		case apiWebhookID.MatchString(r.URL.Path):
+		case adminApiTrackCount.MatchString(r.URL.Path):
+		case adminApiTracks.MatchString(r.URL.Path):
+		default:
+			errStatus(w, http.StatusNotFound, nil, "")
+		}
+	} else {
+		errStatus(w, http.StatusNotImplemented, nil, "")
+	}
+
+}
+
+//Write status header and body with status code, error if exist, and possible extra info
+func errStatus(w http.ResponseWriter, status int, err error, extraInfo string) {
+	if err != nil {
+		http.Error(w, fmt.Sprintf("%s\n%s\n%s", http.StatusText(status), err, extraInfo), status)
+	} else {
+		http.Error(w, fmt.Sprintf("%s\n%s", http.StatusText(status), extraInfo), status)
+	}
+}

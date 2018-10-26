eaee2d63024e538a4d2e66665e3f31d5bd4065e1
CelebornBrian <pkbuer@stud.ntnu.no>
2018-10-26 11:47:27 +0200

    Added method detection and handeling to router

--- router.go
+++ router.go
@@ -72,39 +72,51 @@ func router(w http.ResponseWriter, r *http.Request) {
 
 	//Check if request is GET, POST or DELETE
 	if r.Method == http.MethodGet || r.Method == http.MethodPost || r.Method == http.MethodDelete {
-		//Switch on the request url path and select handler
-		switch {
-		case redirectAPI.MatchString(r.URL.Path):
-			http.Redirect(w, r, "/paragliding/api", http.StatusPermanentRedirect)
-		case apiHandler.MatchString(r.URL.Path):
-			handleAPI(w)
-		case apiTrackHandler.MatchString(r.URL.Path):
-			//The response should have header type json no matter what
-			w.Header().Set("Content-Type", "application/json")
-			if r.Method == http.MethodPost {
-				trackPOST(w, r)
-			} else if r.Method == http.MethodGet {
+		//If GET
+		if r.Method == http.MethodGet {
+			//Switch on the request url path and select handler
+			switch {
+			case redirectAPI.MatchString(r.URL.Path):
+				http.Redirect(w, r, "/paragliding/api", http.StatusPermanentRedirect)
+			case apiHandler.MatchString(r.URL.Path):
+				handleAPI(w)
+			case apiTrackHandler.MatchString(r.URL.Path):
 				trackGET(w, r)
+			case apiTrackIDHandler.MatchString(r.URL.Path):
+				getOne(w, r)
+			case apiTrackIDFieldHandler.MatchString(r.URL.Path):
+				getField(w, r)
+			case apiTicker.MatchString(r.URL.Path):
+				returnTicker(w, r)
+			case apiTickerLatest.MatchString(r.URL.Path):
+				_, err = w.Write([]byte(strconv.FormatInt(getLatest(w), 10)))
+				if err != nil {
+					errStatus(w, http.StatusInternalServerError, err, "Could not write timestamp")
+				}
+			case apiTickerTimestamp.MatchString(r.URL.Path):
+				returnTicker(w, r)
+			case adminAPITrackCount.MatchString(r.URL.Path):
+			case adminAPITracks.MatchString(r.URL.Path):
+			default:
+				errStatus(w, http.StatusNotFound, nil, "")
 			}
-		case apiTrackIDHandler.MatchString(r.URL.Path):
-			getOne(w, r)
-		case apiTrackIDFieldHandler.MatchString(r.URL.Path):
-			getField(w, r)
-		case apiTicker.MatchString(r.URL.Path):
-			returnTicker(w, r)
-		case apiTickerLatest.MatchString(r.URL.Path):
-			_, err = w.Write([]byte(strconv.FormatInt(getLatest(w), 10)))
-			if err != nil {
-				errStatus(w, http.StatusInternalServerError, err, "Could not write timestamp")
+			//Else if POST
+		} else if r.Method == http.MethodPost {
+			switch {
+			case apiTrackHandler.MatchString(r.URL.Path):
+				trackPOST(w, r)
+			case apiWebhookNewTrack.MatchString(r.URL.Path):
+			default:
+				errStatus(w, http.StatusNotFound, nil, "")
 			}
-		case apiTickerTimestamp.MatchString(r.URL.Path):
-			returnTicker(w, r)
-		case apiWebhookNewTrack.MatchString(r.URL.Path):
-		case apiWebhookID.MatchString(r.URL.Path):
-		case adminAPITrackCount.MatchString(r.URL.Path):
-		case adminAPITracks.MatchString(r.URL.Path):
-		default:
-			errStatus(w, http.StatusNotFound, nil, "")
+			//Else means DELETE
+		} else {
+			switch {
+			case apiWebhookID.MatchString(r.URL.Path):
+			default:
+				errStatus(w, http.StatusNotFound, nil, "")
+			}
+
 		}
 	} else {
 		errStatus(w, http.StatusMethodNotAllowed, nil, "")
